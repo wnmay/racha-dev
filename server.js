8cae6755dd7f48304e844ea6f443198c8ff09957
wutthichod <wutthichod@gmail.com>
2025-04-11 10:41:08 +0700

    init and schema

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import authRoute from './routes/auth'
+
+const app = express();
+
+app.use('/auth', auth);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server has started on port: ${PORT}`)
+});
